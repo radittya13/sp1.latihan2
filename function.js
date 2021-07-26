dc006de4e3fdd1951d21b38e82eb200b4336323e
radittya13 <86655614+radittya13@users.noreply.github.com>
2021-07-26 13:09:11 +0700

    Delete function.js

--- function.js
+++
@@ -1,8 +0,0 @@
-var panjang = prompt("masukan nilai panjang");
-var lebar = prompt("masukan nilai lebar");
-panjang = parseInt(panjang);
-lebar = parseInt(lebar);
-var luas; 
-luas = panjang * lebar 
-document.write("hasil luas persegi panjang =" + luas);
-    
