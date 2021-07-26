10bfd036637cb7ba72199ab7128f21c4e50719b2
radittya13 <86655614+radittya13@users.noreply.github.com>
2021-07-26 12:48:40 +0700

    Add files via upload

---
+++ function.js
@@ -0,0 +1,8 @@
+var panjang = prompt("masukan nilai panjang");
+var lebar = prompt("masukan nilai lebar");
+panjang = parseInt(panjang);
+lebar = parseInt(lebar);
+var luas; 
+luas = panjang * lebar 
+document.write("hasil luas persegi panjang =" + luas);
+    
